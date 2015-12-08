f125eb912cc7db0428849bedfeb2659c71be6cab
kunsuoh <kunsuoh@gmail.com>
2015-12-08 12:47:49 -0800

    write histograms

--- MakeHisto/StRoot/StLowPtNpeAnaMaker/StLowPtNpeAnaMaker.cxx
+++ MakeHisto/StRoot/StLowPtNpeAnaMaker/StLowPtNpeAnaMaker.cxx
@@ -64,7 +64,13 @@ Int_t StLowPtNpeAnaMaker::Finish()
 {
     mOutputFile->cd();
     // write histograms
-    
+    for (int i=0 ; i<7 ; i++)
+    for (int j=0 ; j<5 ; j++)
+    for (int k=0 ; k<102 ; k++)
+    {
+        histoAll[i][j][k]->Write();
+    }
+
     
     mOutputFile->Close();
     return kStOK;
