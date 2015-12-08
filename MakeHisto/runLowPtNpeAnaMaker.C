33eb8f9ce798610cdb58588a7beffc7eeb7386c8
kunsuoh <kunsuoh@gmail.com>
2015-12-08 14:32:14 -0800

    modify StRefMult..

--- MakeHisto/runLowPtNpeAnaMaker.C
+++ MakeHisto/runLowPtNpeAnaMaker.C
@@ -38,6 +38,7 @@ void runLowPtNpeAnaMaker(TString npeList="small.list", TString outFileName="test
     
     StPicoDstMaker* picoDstMaker = new StPicoDstMaker(0,npeList,"picoDstMaker");
     StLowPtNpeAnaMaker*  LowPtNpeAnaMaker = new StLowPtNpeAnaMaker("LowPtNpeAnaMaker", picoDstMaker, outFileName.Data());
+    
     // -------------- USER variables -------------------------
     
     // add your cuts here.
@@ -48,8 +49,6 @@ void runLowPtNpeAnaMaker(TString npeList="small.list", TString outFileName="test
     {
         if(iEvent%1==0) cout << "Working on eventNumber " << iEvent << endl;
 
-        float runNumber = picoDstMaker->picoDst()->runId();
-        
         npeChain->Clear();
         int iret = npeChain->Make();
         if (iret)
