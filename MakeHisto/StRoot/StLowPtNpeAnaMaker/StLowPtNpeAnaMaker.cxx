0be5d66205920809fd0eddaf7a43baad13e870e3
kunsuoh <kunsuoh@gmail.com>
2015-12-09 16:10:19 -0800

    rm checks and reject iCent<0 event

--- MakeHisto/StRoot/StLowPtNpeAnaMaker/StLowPtNpeAnaMaker.cxx
+++ MakeHisto/StRoot/StLowPtNpeAnaMaker/StLowPtNpeAnaMaker.cxx
@@ -95,7 +95,7 @@ Int_t StLowPtNpeAnaMaker::Make()
     if (isGoodEvent())
     {
         //cout << "CHECK!!! after isGoodEvent()" << endl;
-        cout << "check!!" << endl;
+        //cout << "check!!" << endl;
         
         float refmult = mPicoEvent->refMult();
         float vZ = mPicoEvent->primaryVertex().z();
@@ -106,6 +106,9 @@ Int_t StLowPtNpeAnaMaker::Make()
 
         refmultcorr->initEvent(refmult, vZ, zdcCoincidenceRate) ;
         iCent = refmultcorr->getCentralityBin6();
+        if (iCent<0) {
+            return kStOK;
+        }
         weight = refmultcorr->getWeight();
 
         UInt_t nTracks = picoDst->numberOfTracks();
@@ -114,15 +117,15 @@ Int_t StLowPtNpeAnaMaker::Make()
         std::vector<unsigned short> idxPicoPartnerEs;
         for (unsigned short iTrack = 0; iTrack < nTracks; ++iTrack)
         {
-            cout << "check!! track" << iTrack <<  endl;
+            //cout << "check!! track" << iTrack <<  endl;
             StPicoTrack const* trk = picoDst->track(iTrack);
             if (!trk) continue;
-            cout << "check!! track" << iTrack <<  endl;
+            //cout << "check!! track" << iTrack <<  endl;
             if (isElectron(trk))
             {
-                cout << "check!! track" << iTrack <<  endl;
+                //cout << "check!! track" << iTrack <<  endl;
                 fillHistogram(trk);
-                cout << "check!! track" << iTrack <<  endl;
+                //cout << "check!! track" << iTrack <<  endl;
                 if (isTaggedElectron(trk)) idxPicoTaggedEs.push_back(iTrack);
             }
             
@@ -240,7 +243,7 @@ bool StLowPtNpeAnaMaker::isGoodElectronPair(StElectronPair const & epair, float 
 //-----------------------------------------------------------------------------
 void StLowPtNpeAnaMaker::fillHistogram(StPicoTrack const * const trk) const
 {
-    cout << "CHECK fillHistogram" << endl;
+    //cout << "CHECK fillHistogram" << endl;
     float ptbin[] = {0, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.6, 0.62, 0.64, 0.66, 0.68, 0.7, 0.72, 0.74, 0.76, 0.78, 0.8, 0.82, 0.84, 0.86, 0.88, 0.9, 0.92, 0.94, 0.96, 0.98, 1, 1.05, 1.1,  1.15,  1.2,  1.25,  1.3,  1.35,  1.4,  1.45,  1.5,  1.55,  1.6,  1.65,  1.7,  1.75,  1.8,  1.85,  1.9,  1.95, 2,  2.1,  2.2,  2.3,  2.4,  2.5,  2.6, 2.7, 2.8, 2.9, 3, 3.1, 3.2, 3.3, 3.4, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5};
     
     float pt = trk->gMom().perp();
@@ -249,9 +252,9 @@ void StLowPtNpeAnaMaker::fillHistogram(StPicoTrack const * const trk) const
     //cout << pt << " " << eta << endl;
 
     float dbeta=0;
-    cout << "CHECK fillHistogram" << endl;
+    //cout << "CHECK fillHistogram" << endl;
     getCalTofTrack(trk, dbeta);
-    cout << "CHECK fillHistogram" << endl;
+    //cout << "CHECK fillHistogram" << endl;
     float nSigmaElectron = trk->nSigmaElectron();
     
     int iPt=0;
@@ -264,13 +267,13 @@ void StLowPtNpeAnaMaker::fillHistogram(StPicoTrack const * const trk) const
         }
     }
     for (int k=0;k<5;k++) if (TMath::Abs(eta) < k*0.1 + 0.1) {iEta=k;break;}
-    cout << "CHECK fillHistogram" << endl;
+    //cout << "CHECK fillHistogram" << endl;
 
-    cout << iCent << " " << iPt << " " << iEta << endl;
+    //cout << iCent << " " << iPt << " " << iEta << endl;
     
     histoAll[iCent][iEta][iPt]->Fill(dbeta,nSigmaElectron,weight);
     histoAll[0][iEta][iPt]->Fill(dbeta,nSigmaElectron,weight);
-    cout << "CHECK fillHistogram" << endl;
+    //cout << "CHECK fillHistogram" << endl;
 
 }
 //--------------------
